fc8afb715ac69826f0def40f2e85af658070b860
Achiote Apricots <aapricot@mi-n5.msk.21-school.ru>
2020-12-03 16:23:41 +0300

    forgotten norm

--- srcs/init_parsed_scene.c
+++ srcs/init_parsed_scene.c
@@ -6,7 +6,7 @@
 /*   By: aapricot <aapricot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 20:37:35 by aapricot          #+#    #+#             */
-/*   Updated: 2020/11/23 21:09:22 by aapricot         ###   ########.fr       */
+/*   Updated: 2020/12/03 16:22:00 by aapricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,24 @@
 #include "texture_manager.h"
 #include "light_manager.h"
 
-/*
-**	За основу взят файл scene.c
-**	Инициализация дефолтных параметров и вызов парсера
-*/
-
-int		init_parsed_scene(t_scene *scene, t_sampler_manager *sampler_manager,
-					   t_res_mngr *resource_manager, char *scene_file)
+int						init_parsed_scene(t_scene *scene,
+	t_sampler_manager *sampler_manager,
+	t_res_mngr *resource_manager, char *scene_file)
 {
-	t_instance_manager *instance_manager;
-	t_parsed_info asset;
+	t_instance_manager	*instance_manager;
+	t_parsed_info		asset;
 
 	instance_manager = &scene->instance_mngr;
 	init_instance_manager(instance_manager);
 	init_light_manager(&scene->light_manager);
-	scene->camera.sampler_id = new_sampler(sampler_manager, rand_jitter, 25, DISK_SAMPLES);
+	scene->camera.sampler_id = new_sampler(sampler_manager,
+	rand_jitter, 25, DISK_SAMPLES);
 	scene->ambient_occluder.color = (t_color){.r = 1.0f, .g = 1.0f, .b = 1.0f};
 	scene->ambient_occluder.ls = 0.05f;
-	scene->ambient_occluder.min_amount = (t_color){ .r = 0.1f, .g = 0.1f, .b = 0.1f };
-	scene->ambient_occluder.sampler_id = new_sampler(sampler_manager, rand_jitter, 100, HEMISPHERE_SAMPLES);
+	scene->ambient_occluder.min_amount =
+	(t_color){ .r = 0.1f, .g = 0.1f, .b = 0.1f };
+	scene->ambient_occluder.sampler_id =
+	new_sampler(sampler_manager, rand_jitter, 100, HEMISPHERE_SAMPLES);
 	scene->ambient_light = (t_light){
 		.type = ambient,
 		.ls = 0.2f,
