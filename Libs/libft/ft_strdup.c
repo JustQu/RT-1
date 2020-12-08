110b7f4ec959a88563417f42fad67ee3faeb7326
alexSolohin <solohinalex1@gmail.com>
2020-12-08 17:52:40 +0200

    ft_strdup leak

--- Libs/libft/ft_strdup.c
+++ Libs/libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmaxima <rmaxima@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 11:24:09 by rmaxima           #+#    #+#             */
-/*   Updated: 2019/09/18 17:10:43 by rmaxima          ###   ########.fr       */
+/*   Updated: 2020/12/08 17:32:54 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ char	*ft_strdup(const char *src)
 		j++;
 	}
 	dest[j] = '\0';
+	free(src);
 	return (dest);
 }
